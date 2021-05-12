0d3083fc68c233d639ab4a9d02531ad7a06b8dcd
alansmathew <alansmathew@mca.ajce.in>
2021-05-12 11:38:53 +0530

    Initial Commit

---
+++ Employee_Location_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Employee_Location_App
+//
+//  Created by Alan S Mathew on 12/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Employee_Location_App/Employee_Location_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Employee_Location_AppApp.swift
+//  Employee_Location_App
+//
+//  Created by Alan S Mathew on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Employee_Location_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
